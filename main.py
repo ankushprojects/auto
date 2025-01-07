2ddff340201c52ed72780012fba9122a008c7171
ankushprojects <154208045+ankushprojects@users.noreply.github.com>
2025-01-07 21:00:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,4 +23,4 @@ async def update_server(background_tasks: BackgroundTasks):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", reload=True)
