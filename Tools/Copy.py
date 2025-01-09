823d4431c307531f28d1d29576a2cc07ccffe41d
ankushprojects <154208045+ankushprojects@users.noreply.github.com>
2025-01-09 21:08:37 +0530

    Update Copy.py

--- Tools/Copy.py
+++ Tools/Copy.py
@@ -57,7 +57,7 @@ def perform_update():
         options = webdriver.ChromeOptions()
         options.add_argument('--start-maximized')  
         options.add_argument('--disable-gpu')
-        # options.add_argument('--headless')
+        options.add_argument('--headless')
         options.add_argument('--no-sandbox') 
         options.add_argument('--disable-dev-shm-usage') 
         options.add_argument('--remote-debugging-port=9222')  
